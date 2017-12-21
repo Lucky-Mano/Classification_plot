727772bce340e9a122b8620497ab792ae5794c68
Lucky-Mano <phatbowie@gmail.com>
2017-12-21 11:23:44 +0900

    コミット漏れを追加

--- Source/main.py
+++ Source/main.py
@@ -8,7 +8,6 @@ from auto_encoder.AE import AutoEncoder, StackedAutoEncoder
 
 mpl.use('TkAgg')
 plt.rcParams['figure.figsize'] = [12, 9]
-BATCH_SIZE = 512
 
 def main():
     """
@@ -28,6 +27,7 @@ def main():
     args = parser.parse_args().__dict__
     neurons = list(map(lambda x: int(x), args.pop('neurons').split(',')))
     epochs = args.pop('epochs')
+    batch_size = args.pop('batch')
 
     (x_train, x_test), (y_train, y_test) = mnist.load_data()
     data = np.concatenate((x_train, y_train)) / 255.0
@@ -58,13 +58,13 @@ def main():
             train_data = data
 
         ae.compile()
-        ae.train(train=train_data, epochs=epochs, batch_size=BATCH_SIZE)
+        ae.train(train=train_data, epochs=epochs, batch_size=batch_size)
         train_data = ae.encoder.predict(train_data)
 
     sae = StackedAutoEncoder(num_neurons)
     sae.load_weight(aes)
     sae.compile()
-    sae.train(data, epochs, BATCH_SIZE)
+    sae.train(data, epochs, batch_size)
     reduced = sae.encoder.predict(data)
 
     for i in range(10):
